3d5b85b5ba35314f9c275a5fff6e1f3fe005a0cc
nyum76 <nyum2024@gmail.com>
2025-06-04 14:31:13 +0900

    ✨ 전역배지 추가

--- js/main.js
+++ js/main.js
@@ -9,9 +9,29 @@ searchEl.addEventListener('click', function () {  /* div.search 요소를 클릭
 searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
-})
+});
 // 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행
 searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
-})
+});
+
+// 페이지 스크롤에 따른 요소 제어
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // Badge 요소 숨기기
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // Badge 요소 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+});
